ee3c74b9a177b8bc3e3e1e86b49f0c06157fbb89
startercoachpro <wh6783@googlemail.com>
2025-08-17 19:25:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'scp-portable-v11';
+const CACHE = 'scp-portable-v12';
 const ASSETS = ['/', '/index.html', '/manifest.webmanifest', '/styles.css', '/app.js', '/config.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
